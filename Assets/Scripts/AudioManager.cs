ca9d27cd68476b5abf9954968e1e57e28cf6ad7b
Nguyen Tran Khanh Kien <nguyenkien264038@gmail.com>
2025-07-09 14:14:31 +0700

    menu game

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+
+    [Header("SFX Clips")]
+    public AudioClip buttonClick;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Giữ lại khi đổi scene
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayButtonClick()
+    {
+        if (sfxSource != null && buttonClick != null)
+            sfxSource.PlayOneShot(buttonClick);
+    }
+
+
+    public void SetMusicVolume(float volume)
+    {
+        bgmSource.volume = volume;
+    }
+
+    public void MuteMusic(bool mute)
+    {
+        bgmSource.mute = mute;
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject TutorialsPanel;
+    public GameObject SettingsPanel;
+
+    [Header("Buttons")]
+    public Button PlayButton;
+    public Button TutorialsButton;
+    public Button SettingsButton;
+    public Button ExitButton;
+    public Button CloseTutorialsButton;
+    public Button CloseSettingsButton;
+
+    [Header("Toggles")]
+    public Toggle MusicToggle;
+    public Toggle SoundToggle;
+
+    [Header("Sliders")]
+    public Slider MusicSlider;
+
+    void Start()
+    {
+        // Gán các sự kiện OnClick
+        PlayButton.onClick.AddListener(PlayGame);
+        TutorialsButton.onClick.AddListener(ShowTutorials);
+        CloseTutorialsButton.onClick.AddListener(CloseTutorials);
+        SettingsButton.onClick.AddListener(ShowSettings);
+        CloseSettingsButton.onClick.AddListener(CloseSettings);
+        ExitButton.onClick.AddListener(ExitGame);
+
+        // Gán sự kiện Toggle và Slider
+        MusicToggle.onValueChanged.AddListener(delegate { ToggleMusic(); });
+        SoundToggle.onValueChanged.AddListener(delegate { ToggleSound(); });
+        MusicSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+
+        // Ẩn panel ban đầu
+        TutorialsPanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+
+        // Khởi tạo trạng thái Toggle và Slider từ AudioManager (nếu có)
+        if (AudioManager.Instance != null)
+        {
+            MusicSlider.value = AudioManager.Instance.bgmSource.volume;
+            MusicToggle.isOn = !AudioManager.Instance.bgmSource.mute;
+        }
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("Play Game Clicked");
+        SceneManager.LoadScene("GamePlay"); // Đổi tên thành scene thực tế
+    }
+
+    public void ShowTutorials()
+    {
+        TutorialsPanel.SetActive(true);
+        SettingsButton.gameObject.SetActive(false);
+        ExitButton.gameObject.SetActive(false);
+    }
+
+    public void CloseTutorials()
+    {
+        TutorialsPanel.SetActive(false);
+        SettingsButton.gameObject.SetActive(true);
+        ExitButton.gameObject.SetActive(true);
+    }
+
+    public void ShowSettings()
+    {
+        SettingsPanel.SetActive(true);
+        ExitButton.gameObject.SetActive(false);
+    }
+
+    public void CloseSettings()
+    {
+        SettingsPanel.SetActive(false);
+        ExitButton.gameObject.SetActive(true);
+    }
+
+    public void ToggleMusic()
+    {
+        Debug.Log("Music is " + (MusicToggle.isOn ? "ON" : "OFF"));
+
+        if (AudioManager.Instance != null)
+            AudioManager.Instance.MuteMusic(!MusicToggle.isOn);
+    }
+
+    public void OnMusicVolumeChanged(float volume)
+    {
+        if (AudioManager.Instance != null)
+            AudioManager.Instance.SetMusicVolume(volume);
+    }
+
+    public void ToggleSound()
+    {
+        Debug.Log("Sound is " + (SoundToggle.isOn ? "ON" : "OFF"));
+        // Thêm xử lý SFX sau nếu bạn có hệ thống SFX
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Game Clicked");
+        Application.Quit();
+    }
+}
