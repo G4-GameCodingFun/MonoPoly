e56a61c41513c4cfbb366e7c270720ffe72ce95e
HuynhNguyenQueTran <tranhnqse183031@fpt.edu.vn>
2025-07-11 08:18:53 +0700

    fix_script

--- Assets/Scripts/CardData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-[System.Serializable]
-public class CardData
-{
-    public string cardName;
-    public Sprite cardSprite;
-    public string cardType; // "CoHoi" ho?c "KhiVan"
-}
